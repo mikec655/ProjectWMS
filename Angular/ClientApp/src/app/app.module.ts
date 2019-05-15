b089c4d40544bc1943aab57a736c662510e00a75
Niek Schoemaker <niekschoemaker@gmail.com>
2019-05-15 12:41:29 +0200

    Revert "Updated to .net core 2.2 and added login and register screen"
    This reverts commit 539c07f27e6716d3f9e4584a300f920a7d386768.

--- Angular/ClientApp/src/app/app.module.ts
+++ Angular/ClientApp/src/app/app.module.ts
@@ -12,7 +12,6 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
-import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -22,8 +21,7 @@ import { RegisterComponent } from './register/register.component';
     CounterComponent,
     FetchDataComponent,
     HelloWorldComponent,
-    AuthenticationComponent,
-    RegisterComponent
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -35,8 +33,7 @@ import { RegisterComponent } from './register/register.component';
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'hello-world', component: HelloWorldComponent },
-      { path: 'login', component: AuthenticationComponent },
-      { path: 'register', component: RegisterComponent}
+      { path: 'login', component: AuthenticationComponent }
     ])
   ],
   providers: [],
