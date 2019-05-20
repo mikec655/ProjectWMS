2a296c0397f6dafeba0a87d8904651a50d976dfc
Niek Schoemaker <niekschoemaker@gmail.com>
2019-05-20 16:32:23 +0200

    Update register.component.ts

--- Angular/ClientApp/src/app/register/register.component.ts
+++ Angular/ClientApp/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, NgModel, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { NgModel, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { MustMatch } from '../_utils/password-match.validator'
 
