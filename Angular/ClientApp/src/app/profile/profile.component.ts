126f3ef8aadbf1a33137e71e87354ff7f8a452ab
gerben bunt <g.bunt@st.hanze.nl>
2019-06-18 15:06:38 +0200

    geen idee

--- Angular/ClientApp/src/app/profile/profile.component.ts
+++ Angular/ClientApp/src/app/profile/profile.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  //template url aanpassen naar een template die opgehaald wordt door authenticatie?
 
+  //als deze variabele: notloggedin is dan veranderd htmllayout.
+  //dus als we in de service checken of het id van de ingelodepersoon gelijkt is aand de pagina dan weergeven we dit?
+  //geen idee of dit handig is tho, misschien beter om 2 componenten te maken.
+  profileview = 'loggedin'
   constructor() { }
 
   ngOnInit() {
   }
-
+  
 }
