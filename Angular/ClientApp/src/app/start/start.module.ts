4cebf8bf82618131fe2581eca57cf74284f94f66
Bernt <berntjanbosma@gmail.com>
2019-06-24 23:32:00 +0200

    Map in sideBar

--- Angular/ClientApp/src/app/start/start.module.ts
+++ Angular/ClientApp/src/app/start/start.module.ts
@@ -4,6 +4,7 @@ import { ProfileSideBarComponent } from './profile-side-bar/profile-side-bar.com
 import { MapSideBarComponent } from './map-side-bar/map-side-bar.component';
 import { StartComponent } from './start/start.component';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [ProfileSideBarComponent, MapSideBarComponent, StartComponent],
