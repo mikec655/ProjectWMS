00f75676811386c67929a63f2851d37d6c50c3ed
gerben bunt <g.bunt@st.hanze.nl>
2019-06-24 11:29:49 +0200

    profile en posts dingen toegevoegd

--- Angular/ClientApp/src/app/profile/profile.module.ts
+++ Angular/ClientApp/src/app/profile/profile.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReviewComponent } from './review/review.component';
 import { ProfileComponent } from './profile.component';
+import { PostComponent } from '../post/post.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ReviewComponent, ProfileComponent],
+  declarations: [ReviewComponent, ProfileComponent, PostComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
