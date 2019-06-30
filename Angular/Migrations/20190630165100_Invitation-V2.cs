6df5fe25b4b80c89826fe0788eb153493e1a6a1b
Niek Schoemaker <niekschoemaker@gmail.com>
2019-06-30 18:51:20 +0200

    Added Migration Invitataion-V2

---
+++ Angular/Migrations/20190630165100_Invitation-V2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Angular.Migrations
+{
+    public partial class InvitationV2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostedAt",
+                table: "Invitations");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InvitationDate",
+                table: "Invitations",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 1,
+                column: "PostedAt",
+                value: new DateTime(2019, 6, 30, 18, 50, 59, 706, DateTimeKind.Local).AddTicks(2634));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "PostedAt",
+                value: new DateTime(2019, 6, 30, 18, 50, 59, 705, DateTimeKind.Local).AddTicks(8945));
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 1,
+                column: "PostedAt",
+                value: new DateTime(2019, 6, 30, 16, 50, 59, 714, DateTimeKind.Utc).AddTicks(2078));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "BirthDate",
+                value: new DateTime(2019, 6, 30, 18, 50, 59, 703, DateTimeKind.Local).AddTicks(3263));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvitationDate",
+                table: "Invitations");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PostedAt",
+                table: "Invitations",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 1,
+                column: "PostedAt",
+                value: new DateTime(2019, 6, 29, 1, 44, 1, 44, DateTimeKind.Local).AddTicks(9283));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "PostedAt",
+                value: new DateTime(2019, 6, 29, 1, 44, 1, 44, DateTimeKind.Local).AddTicks(5331));
+
+            migrationBuilder.UpdateData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 1,
+                column: "PostedAt",
+                value: new DateTime(2019, 6, 28, 23, 44, 1, 53, DateTimeKind.Utc).AddTicks(2821));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "BirthDate",
+                value: new DateTime(2019, 6, 29, 1, 44, 1, 42, DateTimeKind.Local).AddTicks(2453));
+        }
+    }
+}
